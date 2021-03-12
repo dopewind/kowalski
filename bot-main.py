760f470d0f6cc4841e963411cda2f93392fc8782
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-12 23:14:08 +0530

    :alembic: experiments on stuff

--- bot-main.py
+++ bot-main.py
@@ -1,41 +1,26 @@
 # Imports
+import discord
 import discord  # duh
 from discord.ext import commands
 import os
 
 
 # env varables
-apollo_token = os.getenv('apollo_token')
+discord_token = os.getenv('discord_token')
 
 
-client = discord.Client()
+class theClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
 
 
-@client.event
-async def on_message(message):
-    print("- Message received -")
-    print(str(message.content))
-
-    message.content = message.content.lower()
-
-    if message.author == client.user:
-        return
-
-    if str(message.author) == "spacemonk#5679":
-        if message.content == "hello":
-            await message.channel.send("Hello boss")
-        elif message.content == "nice":
-            await message.channel.send("noice")
-    else:
-        message.channel.send("who are you" + message.author + "?")
-
-
-# awaiting argument
-client = commands.Bot(command_prefix="!")
-
-
-@client.command
-async def command_call(ctx, arg):
-    await ctx.send(arg)
-
-client.run(apollo_token)
+client = theClient()
+client.run(str(discord_token))
