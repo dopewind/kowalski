d8a3afe6038d381b96288c4b6ff2ad56a4e3fae9
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-12 23:17:18 +0530

    :sparkles: logging

--- bot-main.py
+++ bot-main.py
@@ -1,12 +1,20 @@
 # Imports
-import discord
 import discord  # duh
 from discord.ext import commands
 import os
+import logging
 
 
 # env varables
 discord_token = os.getenv('discord_token')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 class theClient(discord.Client):
