f2144b0565f52186f2a1f18b0aa26a2a52156d4c
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-13 19:37:10 +0530

    :sparkles: basic stuff

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,27 @@
 // env
 require('dotenv').config();
 
-// online
+// client
 
 const { Client } = require('discord.js');
 const client = new Client();
 
+// event -- ready & loged in
 
+client.on('ready', () => {
+    console.log(`The bot has logged in as ${client.user.tag}`)
+
+
+// event -- message
+
+client.on('message', (message) => {
+    console.log(`${message.author.tag} has send the message \"${message.content}\"`)
+    if (message.content === 'hello'){
+        message.reply("hello there!")
+    }
+});
+});
+
+// login
 client.login(process.env.BOT_TOKEN);
+
