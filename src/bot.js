5bc714543837816092e0cd2c0307d1174531bf64
Restyled.io <commits@restyled.io>
2021-03-13 17:55:17 +0000

    Restyled by clang-format

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // client
 
-const { Client } = require("discord.js");
+const {Client} = require("discord.js");
 const client = new Client();
 
 // constants
@@ -13,36 +13,35 @@ const PREFIX = ">";
 // event -- ready & loged in
 
 client.on("ready", () => {
-  console.log(`The bot has logged in as ${client.user.tag}`); // logs the bot online
+  console.log(
+      `The bot has logged in as ${client.user.tag}`); // logs the bot online
   client.user.setPresence({
-    activity: {
-      name: "my sensei",
-      type: "LISTENING",
-      url: "https://discord.com/users/452762203880292354/",
+    activity : {
+      name : "my sensei",
+      type : "LISTENING",
+      url : "https://discord.com/users/452762203880292354/",
     }, // sets the status
-    status: "online",
+    status : "online",
   }); // shows bot online
 });
 
 // event -- message
 
 client.on("message", (message) => {
-  console.log(
-    `${message.author.tag} has send the message \"${message.content}\"`
-  ); // logs the user who sent the message
+  console.log(`${message.author.tag} has send the message \"${
+      message.content}\"`); // logs the user who sent the message
   if (message.content === "hello") {
     // basic stuff
     message.reply("hello there!");
   }
   if (message.content.startsWith(PREFIX)) {
     // --------------- COMMANDS ------------------
-    const [CMD_NAME, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
+    const [CMD_NAME, ...args] =
+        message.content.trim().substring(PREFIX.length).split(/\s+/);
 
     if (CMD_NAME === "kick") {
-      if (args.length === 0) return message.reply("Please provide an ID");
+      if (args.length === 0)
+        return message.reply("Please provide an ID");
       const member = message.guild.members.cache.get(args[0]);
 
       console.log(member);
