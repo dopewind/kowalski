e227b5bb416daf83ca732b380599f1f11365afee
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-13 21:59:35 +0530

    :sparkles: Now shows status

--- src/bot.js
+++ src/bot.js
@@ -6,21 +6,32 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client();
 
+// constants
+
+const PREFIX = ">";
+
 // event -- ready & loged in
 
 client.on('ready', () => {
-    console.log(`The bot has logged in as ${client.user.tag}`)
-
+    console.log(`The bot has logged in as ${client.user.tag}`);                           // logs the bot online 
+    client.user.setPresence({ 
+    activity: { name: 'my sensei', type: "LISTENING", url: "https://discord.com/users/452762203880292354/" },
+    status: 'online' })
+});
 
 // event -- message
 
 client.on('message', (message) => {
-    console.log(`${message.author.tag} has send the message \"${message.content}\"`)
-    if (message.content === 'hello'){
+    console.log(`${message.author.tag} has send the message \"${message.content}\"`)    // logs the user who sent the message
+    if (message.content === 'hello'){                                                   // basic stuff
         message.reply("hello there!")
     }
+    if (message.content.startsWith(PREFIX)) {                                           // --------------- COMMANDS ------------------
+        const CMD_NAME = message.content.substring(PREFIX.length )
+    }
+
 });
-});
+
 
 // login
 client.login(process.env.BOT_TOKEN);
