ed0b61f536d493e1508a8071ccaf2db7d90b96f9
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-14 00:16:14 +0530

    :art: Improve kick

--- src/bot.js
+++ src/bot.js
@@ -35,22 +35,36 @@ client.on("message", (message) => {
     message.reply("hello there!");
   }
   if (message.content.startsWith(PREFIX)) {
+    //
     // --------------- COMMANDS ------------------
     const [CMD_NAME, ...args] = message.content
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
-
+    //
+    // --------------kick-----------------
     if (CMD_NAME === "kick") {
+      // user needs to have perms
+      if (message.member.hasPermission("KICK_MEMBERS"))
+        return message.reply("You don't have that permission");
+      // ban id
       if (args.length === 0) return message.reply("Please provide an ID");
       const member = message.guild.members.cache.get(args[0]);
-
+      // logs it cuz why not
       console.log(member);
+      // actual thing
       if (member) {
-        member.kick();
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked`))
+          .catch((err) => message.channel.send("I cannot kick that user"));
       } else {
+        // if non-existent
         message.channel.send("That member was not found");
       }
+
+      //
+      //-----------------ban---------------
     } else if (CMD_NAME === "ban") {
       message.channel.send("ban the user");
     }
