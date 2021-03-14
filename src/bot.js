c1a4c9e920ce1237b6fa68ef275bc32714292483
Karthik Vinayan <omendoesstuff@gmail.com>
2021-03-14 17:48:24 +0530

    :sparkles: Kick and ban works

--- src/bot.js
+++ src/bot.js
@@ -26,47 +26,53 @@ client.on('ready', () => {
 
 // event -- message
 
-client.on('message', (message) => {
-  console.log(
-    `${message.author.tag} has send the message "${message.content}"`
-  ); // logs the user who sent the message
-  if (message.content === 'hello') {
-    // basic stuff
-    message.reply('hello there!');
-  }
+client.on('message', async (message) => {
+  // returns if it detects bot msg
+  if (message.author.bot) return;
+  // check for prefix
   if (message.content.startsWith(PREFIX)) {
-    //
-    // --------------- COMMANDS ------------------
     const [CMD_NAME, ...args] = message.content
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
-    //
-    // --------------kick-----------------
+
+    // kick
     if (CMD_NAME === 'kick') {
-      // user needs to have perms
-      if (message.member.hasPermission('KICK_MEMBERS'))
-        return message.reply("You don't have that permission");
-      // ban id
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You do not have permissions to use that command');
       if (args.length === 0) return message.reply('Please provide an ID');
       const member = message.guild.members.cache.get(args[0]);
-      // logs it cuz why not
       console.log(member);
-      // actual thing
       if (member) {
         member
           .kick()
-          .then((member) => message.channel.send(`${member} was kicked`))
-          .catch((_err) => message.channel.send('I cannot kick that user'));
+          .then((member) => message.channel.send(`${member} was kicked.`))
+          .catch(() => {
+            return message.channel.send('I cannot kick that user :(');
+          });
       } else {
-        // if non-existent
         message.channel.send('That member was not found');
       }
 
-      //
-      //-----------------ban---------------
+      // ban
     } else if (CMD_NAME === 'ban') {
-      message.channel.send('ban the user');
+      if (!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply('You do not have permissions to use that command');
+      if (args.length === 0) return message.reply('Please provide an ID');
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send(`${user} was banned successfully`);
+      } catch (err) {
+        console.log(err);
+        message.channel.send(
+          'An error occured. Either I do not have permissions or the user was not found'
+        );
+      }
+    } else if (CMD_NAME === 'announce') {
+      console.log(args);
+      const msg = args.join(' ');
+      console.log(msg);
+      webhookClient.send(msg);
     }
   }
 });
